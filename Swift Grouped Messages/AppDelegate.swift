2afb1afaabb5df8adaf6ae445fcaded162f816fa
masato <masato@m-mbp.local>
2018-10-16 18:58:53 +0800

    initial Commit

--- Swift Grouped Messages/AppDelegate.swift
+++ Swift Grouped Messages/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        window = UIWindow()
+        window?.backgroundColor = UIColor.red
+        window?.makeKeyAndVisible()
+
+        window?.rootViewController = UINavigationController(rootViewController: UIViewController())
+
         return true
     }
 
--- Swift Grouped Messages/ViewController.swift
+++ Swift Grouped Messages/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        navigationItem.title = "Messages"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 
 
